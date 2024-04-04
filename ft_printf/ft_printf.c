99185278579fa8dd515141ce1ae312d6c6db0bcc
Oleh Ruban <oruban@c1s8d36.42wolfsburg.de>
2024-04-04 18:11:31 +0200

    only the code how I have done it on the exam is left

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -33,7 +33,7 @@ int ft_dx_out(unsigned int number, int base, int flag)
     return (i);
 }
 
-int ft_printf(char *str, ...)
+int ft_printf(const char *str, ...)
 {
     va_list ap;
     int i = 0;
@@ -82,3 +82,51 @@ int ft_printf(char *str, ...)
     va_end(ap);
     return (i);
 }
+
+// #include <stdio.h>  // printf
+// #include <limits.h>  // INT_MIN, INT_MAX
+// #define P k += printf
+// #define A k += ft_printf
+
+// int ft_printf(const char *str, ...);
+
+// int main(void)
+// {
+// 	int k = 0;
+	
+// 	P("");
+// 	P("\n");
+// 	P("abcd4321\\\n");
+// 	P("abcd4321\\%%%%\n");
+// 	P("%s\n", "jjlmn");
+// 	P("%s\n", "");
+// 	P("%s\n", NULL);
+// 	P("\n");
+// 	P("%d\n", 0);
+// 	P("%d\n", INT_MIN);
+// 	P("%d\n", INT_MAX);
+// 	P("\n");
+// 	P("%x\n", 0);
+// 	P("%x\n", 999);
+// 	P("%x\n", INT_MAX);
+// 	printf("Total printed out %d characters\n", k);
+	
+// 	k = 0;
+
+// 	A("");
+// 	A("\n");
+// 	A("abcd4321\\\n");
+// 	A("abcd4321\\%%%%\n");
+// 	A("%s\n", "jjlmn");
+// 	A("%s\n", "");
+// 	A("%s\n", NULL);
+// 	A("\n");
+// 	A("%d\n", 0);
+// 	A("%d\n", INT_MIN);
+// 	A("%d\n", INT_MAX);
+// 	A("\n");
+// 	A("%x\n", 0);
+// 	A("%x\n", 999);
+// 	A("%x\n", INT_MAX);
+// 	printf("Total printed out %d characters\n", k);
+// }
